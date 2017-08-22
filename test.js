737e10408542d11738f0423c77bc47e1a622bb96
Motzee <perronm26@protonmail.ch>
2017-08-22 11:38:16 +0200

    Modification du test

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log('Hey !') ;
+const fetch = require(./index.js) ;
+
+fetch("https://dog.ceo/api/breeds/list/all").then(function(response) {
+            console.log(response)
+        }).catch(function(error) {
+            console.error(error);
+        });
+        
